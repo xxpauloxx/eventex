64f9a13f150d1404338fb9fbe618cd680744c280
paulopinda <paulo.pinda@gmail.com>
2016-01-19 21:25:03 -0200

    Remove test without migration

--- eventex/settings.py
+++ eventex/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'test_without_migrations',
+    # 'test_without_migrations',
     'django_extensions',
 
     'eventex.core',
