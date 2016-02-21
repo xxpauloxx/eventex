870ed1ea0b29cc76b18f077e94b679f60437e4e6
paulopinda <paulo.pinda@gmail.com>
2016-02-21 15:32:51 -0300

    Código refatorado.

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -1,10 +1,11 @@
 
 from django import forms
-from eventex.subscriptions.models import Subscription
-from eventex.subscriptions.validators import validate_cpf
 from django.core.exceptions import ValidationError
 
+from eventex.subscriptions.models import Subscription
 
+
+"""
 class SubscriptionForm(forms.Form):
 	name = forms.CharField(label="Nome")
 	cpf = forms.CharField(label="CPF", validators=[validate_cpf])
@@ -21,9 +22,11 @@ class SubscriptionForm(forms.Form):
 		   raise ValidationError('Informe seu e-mail ou telefone.')
 
 		return self.cleaned_data
-	
 """
+
+	
 class SubscriptionForm(forms.ModelForm):
+
 	class Meta:
 		model = Subscription
 		fields = ['name', 'cpf', 'email', 'phone']
@@ -33,7 +36,10 @@ class SubscriptionForm(forms.ModelForm):
 		words = [w.capitalize() for w in name.split()]
 		return ' '.join(words)
 
-	def clean_name(self):
-		name = self.cleaned_data['name']
-		words = [w.capitalize() for w in name.split()]
-		return ' '.join(words)"""
+	def clean(self):
+		self.cleaned_data = super().clean()
+		
+		if not self.cleaned_data.get('email') and not self.cleaned_data.get('phone'):
+		   raise ValidationError('Informe seu e-mail ou telefone.')
+
+		return self.cleaned_data
--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -24,7 +24,8 @@ def create(request):
 		return render(request, 'subscriptions/subscription_form.html',
 			{'form': form})
 
-	subscription = Subscription.objects.create(**form.cleaned_data)
+	subscription = form.save()
+	# subscription = Subscription.objects.create(**form.cleaned_data)
 
 	_send_mail('Confirmação de inscrição',
 			   settings.DEFAULT_FROM_EMAIL,
