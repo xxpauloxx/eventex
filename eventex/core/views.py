c04cdc51300652fa486042e6cbdcc9f175130484
paulopinda <paulo.pinda@gmail.com>
2016-02-27 17:53:19 -0300

    Fix

--- eventex/core/views.py
+++ eventex/core/views.py
@@ -1,12 +1,18 @@
 
 from django.shortcuts import render, HttpResponse, get_object_or_404
+from django.views.generic import ListView 
+
 from eventex.core.models import Speaker, Talk, Course
 
 
+home = ListView.as_view(template_name='index.html', model=Speaker)
+
+
+"""
 def home(request):
 	speakers = Speaker.objects.all()
 	return render(request, "index.html", {'speakers': speakers})
-
+"""
 
 def speaker_detail(request, slug=None):
 	speaker = get_object_or_404(Speaker, slug=slug)
--- eventex/urls.py
+++ eventex/urls.py
@@ -7,9 +7,9 @@ from eventex.core.views import home, speaker_detail, talk_list
 
 urlpatterns = [
 	url(r'^$', home, name='home'),
-	url(r'^inscricao/', include('eventex.subscriptions.urls', 
-								 namespace='subscriptions')),
-
+	
+	# url(r'^$', HomeView.as_view(), name='home'),
+	url(r'^inscricao/', include('eventex.subscriptions.urls', namespace='subscriptions')),
 	url(r'^palestras/$', talk_list, name='talk_list'),
     url(r'^palestrantes/(?P<slug>[\w-]+)/$', speaker_detail, name='speaker_detail'),
     url(r'^admin/', admin.site.urls),
